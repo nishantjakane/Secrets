0ebe821291de4cb849a64f643306457c19f574a9
Nishant212121 <savitarjakane@gmail.com>
2023-01-27 18:21:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true
+})
+mongoose.set('strictQuery', true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields: ['password']})
+
+const User = new mongoose.model("User", userSchema)
+
+// ========= / Route =======
+
+app.get("/", function (req, res) {
+    res.render("home")
+})
+
+// ========== /login Route
+app.get("/login", function (req, res) {
+    res.render("login")
+})
+app.post("/login" , function(req , res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email : username} , function(err , foundUser){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+})
+// ========== /register Route
+
+app.get("/register", function (req, res) {
+    res.render("register")
+})
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("secrets")
+        }
+    })
+})
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000")
+})
